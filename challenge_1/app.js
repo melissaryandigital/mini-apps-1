28038b78f8f80462ffa4b7ac352d9551663cf315
Melissa Ryan <melissaryandigital@gmail.com>
2021-01-25 12:49:44 -0600

    Adds new game functionality that clears board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,26 +4,37 @@
 window.onload = function () {
 
 
+  // Variable to keep track of whose turn it is
   let turn = 1;
-
-  // Keep track of clicks, and each click increment turn
-  // If turn is even, place X
-  // If turn is odd, place 0
 
   // Adds event listener to each table cell
   document.querySelectorAll('#board td').forEach(e =>
     e.addEventListener("click", function () {
 
-      // Check to see which pla
-      // Add X to table cell
-      if (turn % 2 === 1 )  {
+
+      // If turn is even, place X
+      // If turn is odd, place 0
+
+      if (turn % 2 === 1) {
         e.innerHTML = "X";
       } else {
         e.innerHTML = "O";
+
       }
+
       turn++;
-      console.log("turn", turn);
+
     }));
+
+
+  // New game button resets all table cells to empty
+  document.getElementById('newgame').addEventListener('click', function() {
+    document.querySelectorAll('#board td').forEach(el =>
+      {
+        el.innerHTML = '';
+      })
+  });
+
 
   // Create new board by clearing the body and adding new board when button clicked
 
