7b3f0437dcee4226748e3fe1bb1dd8b02b4bc1fa
Melissa Ryan <melissaryandigital@gmail.com>
2021-01-25 15:38:32 -0600

    Breaking interactivity into functions

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,36 +5,58 @@ window.onload = function () {
 
 
   // Variable to keep track of whose turn it is
-  let turn = 1;
+  let Xturn = true;
 
   // Adds event listener to each table cell
-  document.querySelectorAll('#board td').forEach(e =>
-    e.addEventListener("click", function () {
+  document.querySelectorAll('#board td').forEach(el => el.addEventListener("click", handleClick));
+
+  function handleClick(clickedSpace) {
+
+    // If turn is even, place X
+    // If turn is odd, place 0
+
+    let clicked = clickedSpace.target;
+
+    if (Xturn === true) {
+      clicked.innerHTML = "X";
+    } else {
+      clicked.innerHTML = "O";
+    }
+
+    switchTurn();
+  }
 
 
-      // If turn is even, place X
-      // If turn is odd, place 0
+  function switchTurn() {
 
-      if (turn % 2 === 1) {
-        e.innerHTML = "X";
-      } else {
-        e.innerHTML = "O";
+    if (Xturn) {
+      Xturn = false;
+    } else {
+      Xturn = true;
+    }
 
-      }
+  };
 
-      turn++;
+  // Function to check if the space is empty before playing
+  function isEmpty() {
 
-    }));
-
+  };
 
   // New game button resets all table cells to empty
-  document.getElementById('newgame').addEventListener('click', function() {
-    document.querySelectorAll('#board td').forEach(el =>
-      {
-        el.innerHTML = '';
-      })
+  document.getElementById('newgame').addEventListener('click', function () {
+    document.querySelectorAll('#board td').forEach(el => {
+      el.innerHTML = '';
+    })
   });
 
+  let board = {
+
+  };
+
+
+  // Keep track of which td has
+
+  // Determine if there is a win
 
   // Create new board by clearing the body and adding new board when button clicked
 
