760ff9d99e3d76a07639d986371326f2caca23fe
Melissa Ryan <melissaryandigital@gmail.com>
2021-01-25 10:47:18 -0600

    Initialized package, added browser-sync, set up basic table and event listener

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,28 @@
 
 
+// Create new board by adding to the DOM on page load
+window.onload = function () {
+
+
+  // Create new board by clearing the body and adding new board when button clicked
+
+
+  // Place piece
+  document.querySelectorAll('#board td').forEach(e =>
+    e.addEventListener("click", function () {
+
+      // Add X to table cell
+      e.innerHTML = "X";
+      console.log("clicked");
+    }))
+
+  // Check rows
+
+  // Check columns
+
+  // Check diagonals
+
+  // Check for win
+
+};
+
