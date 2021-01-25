5884903af82a6a26cddce6d221bafa2bd0edc294
Melissa Ryan <melissaryandigital@gmail.com>
2021-01-25 11:16:09 -0600

    Tracking turn, alternating XO, and styling

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,17 +4,28 @@
 window.onload = function () {
 
 
-  // Create new board by clearing the body and adding new board when button clicked
+  let turn = 1;
 
+  // Keep track of clicks, and each click increment turn
+  // If turn is even, place X
+  // If turn is odd, place 0
 
-  // Place piece
+  // Adds event listener to each table cell
   document.querySelectorAll('#board td').forEach(e =>
     e.addEventListener("click", function () {
 
+      // Check to see which pla
       // Add X to table cell
-      e.innerHTML = "X";
-      console.log("clicked");
-    }))
+      if (turn % 2 === 1 )  {
+        e.innerHTML = "X";
+      } else {
+        e.innerHTML = "O";
+      }
+      turn++;
+      console.log("turn", turn);
+    }));
+
+  // Create new board by clearing the body and adding new board when button clicked
 
   // Check rows
 
@@ -23,6 +34,12 @@ window.onload = function () {
   // Check diagonals
 
   // Check for win
+
+
+
+
+
+
 
 };
 
