d783b3cdcdfbe52acb218b30bffee3961c346015
Melissa Ryan <melissaryandigital@gmail.com>
2021-01-25 19:18:14 -0600

    Added updateBoardModel and winningCombos

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,9 +3,18 @@
 // Create new board by adding to the DOM on page load
 window.onload = function () {
 
-
-  // Variable to keep track of whose turn it is
   let currentPlayer = 'X';
+  let board = {};
+  let winnigCombos = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ];
 
   // Adds event listener to each table cell
   document.querySelectorAll('#board td').forEach(el => el.addEventListener("click", handleClick));
@@ -13,10 +22,8 @@ window.onload = function () {
   // Handles clicks on the spaces
   function handleClick(clickedSpace) {
 
-    // If turn is even, place X
-    // If turn is odd, place 0
-
     let clicked = clickedSpace.target;
+    let clickedId = clickedSpace.target.id;
 
     if (clicked.innerHTML !== '') {
       isEmpty();
@@ -24,21 +31,24 @@ window.onload = function () {
 
     clicked.innerHTML = currentPlayer;
 
+    updateBoardModel(clickedId, currentPlayer);
+    checkForWin(board, currentPlayer);
+    //checkForTie();
     switchPlayer();
-    updateBoardModel();
-
-  }
-
-  let board = {
-
   };
 
+  // Pass in the space and the player
+  function updateBoardModel(spaceId, player) {
+    board[spaceId] = player;
+    console.log(board);
+  };
 
-  // // Pass in the space and the player
-  // updateBoardModel(){
+  function checkforWin(currentBoard, player) {
 
-  // };
+  // Loop through winning combos
+  // Check to see if currentBoard has currentPlayer in any of these spots
 
+  };
 
   // Handles switching the player's turns
   function switchPlayer() {
@@ -55,6 +65,8 @@ window.onload = function () {
   // Checks if the space is empty before playing
   function isEmpty() {
     document.getElementById('text').innerHTML = 'Someone has already picked that space, please choose another!';
+
+    // Need to remove this text
   };
 
 
@@ -64,29 +76,5 @@ window.onload = function () {
       el.innerHTML = '';
     })
   });
-
-
-
-
-  // Keep track of which td has
-
-  // Determine if there is a win
-
-  // Create new board by clearing the body and adding new board when button clicked
-
-  // Check rows
-
-  // Check columns
-
-  // Check diagonals
-
-  // Check for win
-
-
-
-
-
-
-
 };
 
