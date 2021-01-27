8677c3ee730cea2eead96fa07f99050ac7558a52
Melissa Ryan <melissaryandigital@gmail.com>
2021-01-26 19:41:56 -0600

    Creates form, basic server setup, logs POSTed data

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,6 @@
 
+// Prevent default on form submit
 
+document.querySelector('#json-form').addEventListener('click', function(event) {
+  event.preventDefault();
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// })
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('client'));
+
+app.listen(port, () => {
+  console.log('Example app listening at http://localhost:${port}');
+});
+
+app.get('/', (req, res) => {
+  if (err)  {
+    res.status(400);
+    throw err;
+  }
+  res.status(200);
+});
+
+app.post('/upload_json', (req, res) => {
+
+  let data = req.body;
+
+  console.log(data);
+  console.log('got a post request from the index page');
+
+  res.status(200);
+  res.redirect('/');
+});
