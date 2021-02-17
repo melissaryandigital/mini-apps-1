7a8476603181b1f60415560a2abc9ef853eecf42
Melissa Ryan <melissaryandigital@gmail.com>
2021-02-16 18:17:53 -0600

    Adds basic mongodb schema, mongoose pkg, and renders homepage and form 1

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,16 +1,69 @@
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      page: 'checkout'
+    }
+
+    this.startCheckout = this.startCheckout.bind(this);
+  }
+
+
+
+  // Whats all the stuff we need to do?
+  // Send a post request to a different endpoint? Or just one endpoint and update the record based on ID?
+  // Look up mongodb find by id and update record
+
+
+  componendDidMount() {
+
+  }
+
+  startCheckout(e) {
+    e.preventDefault();
+    console.log('startCheckout');
+    console.log()
+    this.setState({
+      page: 'f1'
+    });
+
+  }
+
+  updatePage() {
+
   }
 
 
   render() {
-    return (<div>
-        <h2>Checkout Process!</h2>
-        <div>Test</div>
-      </div>);
-  }
 
+    // onSubmit change the state
+    // render forms based on state
+    //
+
+    if (this.state.page === 'checkout') {
+      return (<div>
+        <h2>Checkout Process</h2>
+        <div><button onClick={this.startCheckout}>Checkout</button></div>
+      </div>);
+    }
+    if (this.state.page === 'f1') {
+
+      return (<div>
+        <div>User Info</div>
+        <form>
+          <label>Name:</label><br />
+          <input type="text" id="name" value=""></input><br /><br />
+          <label>Email:</label><br />
+          <input type="text" id="email" value=""></input><br /><br />
+          <label>Password:</label><br />
+          <input type="text" id="password" value=""></input><br /><br />
+          <button onClick={this.}></button>
+        </form>
+      </div>)
+    }
+
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,10 +2,44 @@ const express = require('express');
 const app = express();
 const port = 3333;
 
-
+// Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/checkout', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Mongoose connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Mongoose!');
+});
+
+// Mongoose schema
+const orderSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  address1: String,
+  address2: String,
+  city: String,
+  state: String,
+  zip: Number,
+  phone: Number,
+  cardNumber: Number,
+  expiry: Number,
+  CVV: Number,
+  billingZip: Number
+});
+
+
+// Mongoose model
+const Order = mongoose.model('Order', orderSchema);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
