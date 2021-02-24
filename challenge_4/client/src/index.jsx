0e5533d99de3a8bf9c88ca2925640bcc09fcacaa
Melissa Ryan <melissaryandigital@gmail.com>
2021-02-24 09:58:59 -0600

    Updates to actual size of connect four board, updated CSS grid

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -23,27 +23,61 @@ class App extends React.Component {
 
 
   render() {
+
+    // Refactor DRY dynamically create board later
+
     return (
       <div>
         <div className="grid-container">
-          <Square x={0} y={0} handleClick={this.handleClick}/>
-          <Square x={1} y={0} handleClick={this.handleClick}/>
-          <Square x={2} y={0} handleClick={this.handleClick}/>
-          <Square x={3} y={0} handleClick={this.handleClick}/>
-          <Square x={0} y={1} handleClick={this.handleClick}/>
-          <Square x={1} y={1} handleClick={this.handleClick}/>
-          <Square x={2} y={1} handleClick={this.handleClick}/>
-          <Square x={3} y={1} handleClick={this.handleClick}/>
-          <Square x={0} y={2} handleClick={this.handleClick}/>
-          <Square x={1} y={2} handleClick={this.handleClick}/>
-          <Square x={2} y={2} handleClick={this.handleClick}/>
-          <Square x={3} y={2} handleClick={this.handleClick}/>
-          <Square x={0} y={3} handleClick={this.handleClick}/>
-          <Square x={1} y={3} handleClick={this.handleClick}/>
-          <Square x={2} y={3} handleClick={this.handleClick}/>
-          <Square x={3} y={3} handleClick={this.handleClick}/>
-          </div>
-        </div>    )
+          <Square x={0} y={0} handleClick={this.handleClick} />
+          <Square x={1} y={0} handleClick={this.handleClick} />
+          <Square x={2} y={0} handleClick={this.handleClick} />
+          <Square x={3} y={0} handleClick={this.handleClick} />
+          <Square x={4} y={0} handleClick={this.handleClick} />
+          <Square x={5} y={0} handleClick={this.handleClick} />
+          <Square x={6} y={0} handleClick={this.handleClick} />
+
+          <Square x={0} y={1} handleClick={this.handleClick} />
+          <Square x={1} y={1} handleClick={this.handleClick} />
+          <Square x={2} y={1} handleClick={this.handleClick} />
+          <Square x={3} y={1} handleClick={this.handleClick} />
+          <Square x={4} y={1} handleClick={this.handleClick} />
+          <Square x={5} y={1} handleClick={this.handleClick} />
+          <Square x={6} y={1} handleClick={this.handleClick} />
+
+          <Square x={0} y={2} handleClick={this.handleClick} />
+          <Square x={1} y={2} handleClick={this.handleClick} />
+          <Square x={2} y={2} handleClick={this.handleClick} />
+          <Square x={3} y={2} handleClick={this.handleClick} />
+          <Square x={4} y={2} handleClick={this.handleClick} />
+          <Square x={5} y={2} handleClick={this.handleClick} />
+          <Square x={6} y={2} handleClick={this.handleClick} />
+
+          <Square x={0} y={3} handleClick={this.handleClick} />
+          <Square x={1} y={3} handleClick={this.handleClick} />
+          <Square x={2} y={3} handleClick={this.handleClick} />
+          <Square x={3} y={3} handleClick={this.handleClick} />
+          <Square x={4} y={3} handleClick={this.handleClick} />
+          <Square x={5} y={3} handleClick={this.handleClick} />
+          <Square x={6} y={3} handleClick={this.handleClick} />
+
+          <Square x={0} y={4} handleClick={this.handleClick} />
+          <Square x={1} y={4} handleClick={this.handleClick} />
+          <Square x={2} y={4} handleClick={this.handleClick} />
+          <Square x={3} y={4} handleClick={this.handleClick} />
+          <Square x={4} y={4} handleClick={this.handleClick} />
+          <Square x={5} y={4} handleClick={this.handleClick} />
+          <Square x={6} y={4} handleClick={this.handleClick} />
+
+          <Square x={0} y={4} handleClick={this.handleClick} />
+          <Square x={1} y={4} handleClick={this.handleClick} />
+          <Square x={2} y={4} handleClick={this.handleClick} />
+          <Square x={3} y={4} handleClick={this.handleClick} />
+          <Square x={4} y={4} handleClick={this.handleClick} />
+          <Square x={5} y={4} handleClick={this.handleClick} />
+          <Square x={6} y={4} handleClick={this.handleClick} />
+        </div>
+      </div>)
   }
 }
 
